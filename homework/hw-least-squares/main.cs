0dbc8661af00abfcc50e6abaa1ad2f7b234f1995
soflilia <202005376@post.au.dk>
2024-03-08 13:30:04 +0100

    I keep on

--- homework/hw-least-squares/main.cs
+++ homework/hw-least-squares/main.cs
@@ -8,7 +8,11 @@ class Hello{
         vector y1 = new vector("117,100,88,72,53,29.5,25.2,15.2,11.1");
         vector lny = y1.map(Math.Log);
         vector dy1 = new vector("6,5,4,4,4,3,3,2,2");
-        Func<double , double >[] fs = [(double x) => 1.0 , (double x) => -x];
+        Func<double , double> [] fs = new Func<double,double> [2];
+        fs[0] = (double x) => 1;
+        fs[1] = (double x) => -x;
+        //System.Console.Write($"{fs[0](3)}\n");
+        lsfit fit_decay = new lsfit(fs, time, lny, dy1);
 
     }
 }
