c67e74bd58fa25a062fcf9c6826f877559d125f5
soflilia <202005376@post.au.dk>
2024-05-17 15:43:04 +0200

    montecarlo B

--- homework/MonteCarlo/MC.cs
+++ homework/MonteCarlo/MC.cs
@@ -4,6 +4,7 @@ using static System.Math;
 
 
 public class MonteCarlo{
+    static int[] bases = {2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61};
     public (double,double) plainmc(Func<vector,double> f,vector a,vector b,int N){
         int dim=a.size; double V=1; 
         for(int i=0;i<dim;i++){V*=b[i]-a[i];}
@@ -17,5 +18,55 @@ public class MonteCarlo{
             double mean=sum/N, sigma=Sqrt(sum2/N-mean*mean);
             var result=(mean*V,sigma*V/Sqrt(N));
             return result;
+    }
+
+    public (double,double) quasimc(Func<vector,double> f,vector a,vector b,int N){
+        int dim = a.size; double V=1;
+        for(int i=0; i<dim; i++){V*=b[i]-a[i];} // så V er et slags areal/rumfang
+        var x = new vector(dim);
+        double sum=0,sum2=0;
+        var x_next = new vector(dim);
+        double sum_next=0,sum2_next=0;
+        for(int i=0; i<N; i++){
+            for(int k=0;k<dim;k++){
+                x[k]=a[k]+halton(i,dim)[k]*(b[k]-a[k]);
+                x_next[k]= a[k]+halton(i,dim,1)[k]*(b[k]-a[k]);}
+            //initierer værdier
+            double fx = f(x); 
+            double fx_next=f(x_next);
+            sum+=fx;
+            sum_next+=fx_next;
+            sum2+=fx*fx;
+            sum2_next+=fx_next*fx_next;
+        }
+        double result = sum/N*V;
+        double result_next = sum_next/N*V;
+        return (result, Abs(result-result_next));
+
+
+    }
+
+    public static double corput ( int n , int b){
+        //spytter alle mulige værdier ud mellem 0 og 1
+        double q=0;
+        double bk=(double)1.0/b ;
+        while (n>0){ 
+        q += (n % b)* bk ;
+        n /= b ;
+        bk /= b ; 
+        //Error.Write($"{q}");
+        }
+        return q ; 
+    }
+
+    public static vector halton( int n, int d, int sequence=0){
+        vector halton = new vector(d);
+        if( d<bases.Length){
+        for(int i=0; i<d; i++){
+            if(sequence==0) halton[i]=corput(n,bases[i]);
+            if(sequence==1) halton[i]=corput(n,bases[i+d]);}
+        }
+        else{Error.Write("wrong dimensions \n");}
+        return halton;
     }
 }
--- homework/MonteCarlo/main.cs
+++ homework/MonteCarlo/main.cs
@@ -10,26 +10,28 @@ public class MC{
 
         double r_1 = 1.0;
         Func <vector,double> circle = x => {
-            double r = 1.0;
-            if(Pow(x[0],2)+Pow(x[1],2) <= Pow(r,2)){return 1;}
+            if(x.norm() <= 1){return 1;}
             return 0;
             };
         vector a_1 = new vector(-r_1,-r_1);
         vector b_1 = new vector(r_1,r_1);
         //Error.Write($"{circle(b_1)}\n");
-        int N_1 = 300000;
+        int N_1 = 1000000;
         MonteCarlo instance = new MonteCarlo();
         var (result_1,error_1) = instance.plainmc(circle,a_1,b_1,N_1);
         Error.Write($"Result with N={N_1}: {result_1}, Error: {error_1}\n");
+        
 
         //OK nu laver du et plot af error som funktion af N:
-        for(int N = 30; N<100000; N+=10000){
+        for(int N = 30; N<500000; N+=10000){
+            var (est_result_qr,est_error_qr) = new MonteCarlo().quasimc(circle,a_1,b_1,N);
             var (est_result,est_error) = new MonteCarlo().plainmc(circle,a_1,b_1,N);
             double act_error = Abs(PI-est_result);
-            Write($"{N} {est_error} {act_error}\n");
+            double act_error_qr = Abs(PI-est_result_qr);
+            Write($"{N} {est_error} {act_error} {est_error_qr} {act_error_qr}\n");
         }
         Write($"\n\n");
-        for(int N = 30; N<100000; N+=10000){Write($"{N} {1.0/Sqrt(N)}\n");}
+        for(int N = 30; N<500000; N+=10000){Write($"{N} {1.0/Sqrt(N)}\n");}
         //snabel-a gør dig i stand til at splitte string i flere linjer
         Error.Write("The actual error is larger than 1/sqrt(N) for small N,\n"+
         "then converges towards 1/sqrt(N) for larger N>10 000 \n");
@@ -40,10 +42,17 @@ public class MC{
         Func <vector,double> cosin = x => {
             return 1/(Pow(PI,3) * (1-Cos(x[0])*Cos(x[1])*Cos(x[2])));};
 
-        int N_2 = 100000;
+        int N_2 = 1000000;
         var (result_2,error_2) = new MonteCarlo().plainmc(cosin,a_2,b_2,N_2);
         Error.Write($"Result with N={N_2}: {result_2}, Error: {error_2}\n");
 
 
+        //OPG B
+
+        var (result_qr,error_qr) = instance.quasimc(circle,a_1,b_1,N_1);
+        Error.Write($"Result with quasi random sequence N={N_1}: {result_qr}, Error: {error_qr}\n");
+
     }
+
+    
 }
