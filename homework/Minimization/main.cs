6c3f4605261ec37b11f94df1954782ba86f861e3
soflilia <202005376@post.au.dk>
2024-06-27 15:33:22 +0200

    minimization part c

--- homework/Minimization/main.cs
+++ homework/Minimization/main.cs
@@ -3,25 +3,35 @@ using System;
 using static genlist<double>;
 using static System.Math;
 using static System.Console;
+using System.Diagnostics;
 
 
 
 class minimization{
     public static void Main(){
+        // OPG A 
+        Error.WriteLine("OPG A...\n");
         
         vector start = new vector(2,3);
+        Stopwatch timer = new Stopwatch();
+        timer.Start();
         MIN Rosen = new MIN(Rosenbrocks,start);
+        timer.Stop();
         Error.Write("Computing minima at start point = (2,3)...\n");
         Error.Write($"Rosenbrucks function: n = {Rosen.count} iterations, value = {Rosen.value_min}\n");
         Error.Write($"(x,y) = ({Rosen.min_point[0]},{Rosen.min_point[1]})\n");
 
+        Stopwatch timer1 = new Stopwatch();
+        timer1.Start();
         MIN Himmel = new MIN(Himmelblau,start);
+        timer1.Stop();
         Error.Write($"Himmelblau function: n = {Himmel.count} iterations, value = {Himmel.value_min}\n");
         Error.Write($"(x,y) = ({Himmel.min_point[0]},{Himmel.min_point[1]})\n");
+        Error.Write("\n\n");
         
         // OPG B
 
-
+        Error.WriteLine("OPG B...\n");
         var energy = new genlist<double>();
         var cross = new genlist<double>();
         var error  = new genlist<double>();
@@ -47,12 +57,47 @@ class minimization{
         //Jeg har fundet at mange iterations ikke gør noget når den sidder fast, istedet sætter jeg accuracy op
         MIN A1 = new MIN(from_many_to_1,start_gæt, 0.00001, 400);
 
-        Error.Write($"My values are: n = {A1.count} iterations, value = {A1.value_min}\n");
+        Error.Write($"My values for the Breit Wigner fit are: n = {A1.count} iterations, value = {A1.value_min}\n");
         Error.Write($"and the params are m= {A1.min_point[0]}, gamma= {A1.min_point[1]} A = {A1.min_point[2]}\n");
         
         for(double i=100; i<160 ;i+=0.5){
             Write($"{i} {breit_wigner(i,A1.min_point[0],A1.min_point[1],A1.min_point[2])}\n");
         }
+        //OPG C
+        Error.Write("\n\n");
+        Error.WriteLine("OPG C... NOW WITH CENTRAL DIFFERENTIATION\n");
+        Error.Write("Computing minima at start point = (2,3)...\n");
+
+
+
+        Stopwatch timer2 = new Stopwatch();
+        timer2.Start();
+        MIN Rosen1 = new MIN(Rosenbrocks,start, central:true);
+        timer2.Stop();
+
+        Console.Error.Write($"Rosenbrocks central:\n");
+        Error.Write($"Rosenbrucks function: n = {Rosen1.count} iterations, value = {Rosen1.value_min}\n");
+        Error.Write($"(x,y) = ({Rosen1.min_point[0]},{Rosen1.min_point[1]})\n");
+        Console.Error.Write($"central: {timer2.Elapsed.TotalMilliseconds} ms \n");
+        Console.Error.Write($"forward: {timer.Elapsed.TotalMilliseconds} ms \n");
+
+        Stopwatch timer3 = new Stopwatch();
+        timer3.Start();
+        MIN Himmel1 = new MIN(Himmelblau,start,central:true);
+        timer3.Stop();
+        Console.Error.Write($"Himmelblau central:\n");
+        Error.Write($"Himmelblau function: n = {Himmel1.count} iterations, value = {Himmel1.value_min}\n");
+        Error.Write($"(x,y) = ({Himmel1.min_point[0]},{Himmel1.min_point[1]})\n");
+        Console.Error.Write($"central: {timer3.Elapsed.TotalMilliseconds} ms \n");
+        Console.Error.Write($"forward: {timer1.Elapsed.TotalMilliseconds} ms \n");
+        Error.Write("\n\n");
+
+        Console.Error.Write("Conclusion...\n");
+        Console.Error.Write("it seems central derivative method is slightly better, because the resulting minima are smaller and thus\n");
+        Console.Error.Write("the found values (x,y) must be equaly closer to the true value. We also have a time difference in favour\n");
+        Console.Error.Write("of the central algorithm implementation.\n");
+
+
         }// main stops
 
 
--- homework/Minimization/min.cs
+++ homework/Minimization/min.cs
@@ -32,39 +32,69 @@ public class MIN{
 	Func<vector,double> f, /* objective function */
     vector x_start, /* starting point */
 	double acc=1e-3,  /* accuracy goal, on exit |∇φ| should be < acc */
-    double max_count = 1000 // sæt et nyt hvis du vil
+    double max_count = 1000, // sæt et nyt hvis du vil
+    bool central = false
     ){
     count = 0;
     double λmin = 1/1024;
     vector x = x_start.copy();
 
-	do{ /* Newton's iterations */
-		var f_grad= gradient(f,x);
-        count +=1;
-		if(f_grad.norm() < acc){
-            //Error.Write("gradient nonexistent!\n");
-            //Error.Write($"={f_grad.norm()}\n");
-            break;
-            }; /* good step: accept */
-		var H = hessian(f,x);
-        // decomposition of H(x) dx = d phi(x) i pdf
-		(matrix Q,matrix R) = QRGS.decomp(H);   /* QR decomposition */
-		vector dx = QRGS.solve(Q,R,-f_grad); /* Newton's step */
+    if(central==false){
+        do{ /* Newton's iterations */
+            var f_grad= gradient(f,x);
+            count +=1;
+            if(f_grad.norm() < acc){
+                //Error.Write("gradient nonexistent!\n");
+                //Error.Write($"={f_grad.norm()}\n");
+                break;
+                }; /* good step: accept */
+            var H = hessian(f,x);
+            // decomposition of H(x) dx = d phi(x) i pdf
+            (matrix Q,matrix R) = QRGS.decomp(H);   /* QR decomposition */
+            vector dx = QRGS.solve(Q,R,-f_grad); /* Newton's step */
 
-		double λ=1,fx=f(x);
-        if (Double.IsNaN(fx)) {Error.Write($"is Nan inside minimize, iteration no: {count}");
-        fx = 0;};
-		do{ /* linesearch */
-			if( f(x+λ*dx) < fx ){
-            //Error.Write("good step!\n");
-            break;
-            }; /* good step: accept */
-			λ/=2;
-		}while(λ > λmin);
-		x+=λ*dx;
-	}while(count<max_count);
-    value_min = f(x);
-	min_point = x;
+            double λ=1,fx=f(x);
+            if (Double.IsNaN(fx)) {Error.Write($"is Nan inside minimize, iteration no: {count}");
+            fx = 0;};
+            do{ /* linesearch */
+                if( f(x+λ*dx) < fx ){
+                //Error.Write("good step!\n");
+                break;
+                }; /* good step: accept */
+                λ/=2;
+            }while(λ > λmin);
+            x+=λ*dx;
+        }while(count<max_count);
+        value_min = f(x);
+        min_point = x;
+    }
+    else{
+        do{ /* Newton's iterations */
+            (matrix H, vector f_grad) = hessian_central(f,x);
+            count +=1;
+            if(f_grad.norm() < acc){
+                break;
+                }; /* good step: accept */
+
+            // decomposition of H(x) dx = d phi(x) i pdf
+            (matrix Q,matrix R) = QRGS.decomp(H);   /* QR decomposition */
+            vector dx = QRGS.solve(Q,R,-f_grad); /* Newton's step */
+
+            double λ=1,fx=f(x);
+            if (Double.IsNaN(fx)) {Error.Write($"is Nan inside minimize, iteration no: {count}");
+            fx = 0;};
+            do{ /* linesearch */
+                if( f(x+λ*dx) < fx ){
+                //Error.Write("good step!\n");
+                break;
+                }; /* good step: accept */
+                λ/=2;
+            }while(λ > λmin);
+            x+=λ*dx;
+        }while(count<max_count);
+        value_min = f(x);
+        min_point = x;
+    }
     }//newton
 
     public static matrix hessian(Func<vector,double> f,vector x){
@@ -82,5 +112,63 @@ public class MIN{
 	return (H+H.T)/2; // you think?
     }
 
+
+
+//NY HEASSIAN OG NY GRADIENT
+
+    public static (matrix,vector) hessian_central(Func<vector,double> f,vector x){
+	matrix H = new matrix(x.size);
+    //adopter notation med at f1 = first derivative og f2 = second derivative
+	(vector f1, vector f2_samex) = gradient_central(f,x);
+    vector x1 = x.copy();
+	for(int j=0;j<x.size;j++){
+		double dxj = Max(Abs(x[j]),1)*Pow(2,-13); /* for numerical gradient */
+		x[j] += dxj;
+        x1[j] -= dxj;
+        H[j,j] = f2_samex[j];
+		for(int k=0;k<x.size;k++) {
+            if (j != k){
+                // før _ betyder minus, efter _ betyder plus, så f_jk = f( x + dxj + dxk)
+                double dxk = Max(Abs(x[k]),1)*Pow(2,-23);
+                x[k] += dxk;
+                x1[k] +=dxk;
+                double f_jk = f(x);
+                double fj_k = f(x1);
+                x[k] -= 2*dxk;
+                x1[k] -= 2*dxk;
+                double fk_j = f(x);
+                double fjk_ = f(x1);
+                x[k] += dxk;
+                x1[k] += dxk;
+                H[j,k] = (f_jk - fj_k - fk_j + fjk_)/(4*dxj*dxk);
+            }
+        }
+		x[j]-=dxj;
+        x1[j] += dxj;
+	}
+	//return H;
+	return ((H+H.T)/2, f1 ); // you think?
+    }
+
+
+    public static (vector, vector) gradient_central(Func<vector,double> f, vector x){
+        vector grad = new vector(x.size);
+        vector diff_grad_samex = new vector(x.size);
+        double fx= f(x);
+        for (int i=0; i< x.size; i++){
+            double dx = Max(Abs(x[i]),1)*Pow(2,-24);
+            x[i]+= dx;
+            double f_plus_dx = f(x);
+            x[i]-= 2*dx;
+            double f_minus_dx = f(x);
+            if (double.IsNaN(f_plus_dx)|| double.IsNaN(f_minus_dx)) {
+                throw new Exception($"NaN detected in gradient calculation at index {i}, x = {x}");}
+            grad[i] = (f_plus_dx - f_minus_dx)/(2*dx);
+            diff_grad_samex[i] = (f_plus_dx - 2*fx + f_minus_dx) / Pow(dx,2);
+            x[i]+= dx;
+        }
+        //grad.print("grad");
+        return (grad, diff_grad_samex);
+    }
 
 }
